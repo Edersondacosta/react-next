71c63bd0b90ca564a63e5873850077e3bd454d47
Edersondacosta <34005488+Edersondacosta@users.noreply.github.com>
2025-07-19 23:48:03 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -24,25 +24,25 @@ export default async function Main(){
     //Os dados vem em json, estamos descompactando os dados json lista de objeto no exemplo [ {
     //usei cons
         <div className="flex bg-gray-100">
-            <div className="flex w-100 bg-gray-800 text-amber-50 h-screen">
+            <div className="flex fixed top-0 left-0 w-60 bg-gray-800 text-amber-50 h-screen">
                 <nav>
-                    <p>Manu</p>
+                    <p className="ml-1 pt-10 text-2xl">Menu</p>
                     <p className="ml-5 text-sm pt-5">Início</p>
                     <p className="ml-5 text-sm pt-5">Produtos</p>
                     <p className="ml-5 text-sm pt-5">Categorias</p>
                     <p className="ml-5 text-sm pt-5 ">Contato</p>
                 </nav>
             </div>
-            <div className="flex flex-wrap m-15 shadow-2xs ">
+            <div className="flex flex-wrap m-15 shadow-2xs ml-85">
                 {data.map((item,i)=>(
-                <div className="flex flex-col m-2 w-50 h-70 bg-white rounded-2xl" key={(i)}>{/*É preciso definir uma key, para a div ser única*/}
-                        <p className="flex justify-center"><img className="w-15 h-20 mt-2" src={item.image} alt="Imagem"/> </p>
-                        <p className="m-2 text-sm font-bold h-10">{item.title.slice(0,30)} </p>
-                        <p className="text-green-600 ">U$ {item.price} </p>
-                        <p className="text-sm flex-wrap h-10">{item.description.slice(0,25)} </p>
+                <div className="flex flex-col m-2 w-70 h-90 bg-white rounded-2xl shadow-2xl" key={(i)}>{/*É preciso definir uma key, para a div ser única*/}
+                        <p className="mb-5 flex justify-center hover:opacity-30"><img className="w-25 h-30 mt-2" src={item.image} alt="Imagem"/> </p>
+                        <p className="m-1 font-bold h-10 text-1xl">{item.title.slice(0,50)} </p>
+                        <p className="text-green-600 m-1 font-bold">U$ {item.price.toFixed(2)} </p>
+                        <p className="text-sm flex-wrap h-10 m-1">{item.description.slice(0,90)} </p>
                         {/* .slice(0,25) limita o texto de uma variável*/}
-                        <p className="flex justify-center mt-10"><button className="bg-blue-700 rounded w-40 text-amber-50 center ">Ver Mais</button></p>
-
+                        <p className="cursor flex justify-center mt-10 m-1"><a href="https://www.google.com"><button className="bg-blue-700 rounded-2xl w-50 h-10 text-amber-50 center cursor-pointer">Ver Mais</button></a></p>
+                        
                 </div>
                 ))} 
             </div>
