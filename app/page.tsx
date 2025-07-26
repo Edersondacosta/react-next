c6e9cf7770b630d085ccd2c86027e6f672abb960
Edersondacosta <34005488+Edersondacosta@users.noreply.github.com>
2025-07-26 09:33:51 -0300

    Ecommerce com API

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 // "use client"
 import { useState } from "react"
+import Image from "next/image";
+
 
 type userProps = {
         id:number
@@ -23,8 +25,11 @@ export default async function Main(){
    
     //Os dados vem em json, estamos descompactando os dados json lista de objeto no exemplo [ {
     //usei cons
-        <div className="flex bg-gray-100">
-            <div className="flex fixed top-0 left-0 w-60 bg-gray-800 text-amber-50 h-screen">
+
+         <div className="flex bg-gray-200">
+           
+                     
+            <div className="flex fixed top-0 left-0 w-60 bg-gray-900 text-amber-50 h-screen">
                 <nav>
                     <p className="ml-1 pt-10 text-2xl">Menu</p>
                     <p className="ml-5 text-sm pt-5">Início</p>
@@ -33,7 +38,24 @@ export default async function Main(){
                     <p className="ml-5 text-sm pt-5 ">Contato</p>
                 </nav>
             </div>
-            <div className="flex flex-wrap m-15 shadow-2xs ml-85">
+<div className="flex flex-col">
+
+
+
+<div className="flex justify-center mt-5 ml-55 font-bold text-6xl mb-5 p-0">
+                <h1>Ederson's Shop</h1>       
+           </div>
+            <div className="flex h-30 justify-center mt-0 transform transition duration-500 hover:scale-110 hover:brightness-110 hover:blur-xs ">
+                <Image
+                        src="/banner.png"
+                        alt="Imagem do banner"
+                        width={600}
+                        height={300}
+                        className="ml-55"
+            />       
+           </div>
+           
+                   <div className="flex flex-wrap m-15 shadow-2xs ml-85 mt-0">
                 {data.map((item,i)=>(
                 <div className="flex flex-col m-2 w-70 h-90 bg-white rounded-2xl shadow-2xl" key={(i)}>{/*É preciso definir uma key, para a div ser única*/}
                         <p className="mb-5 flex justify-center hover:opacity-30"><img className="w-25 h-30 mt-2" src={item.image} alt="Imagem"/> </p>
@@ -46,6 +68,9 @@ export default async function Main(){
                 </div>
                 ))} 
             </div>
+            </div>
+
+
         </div>
  
 
